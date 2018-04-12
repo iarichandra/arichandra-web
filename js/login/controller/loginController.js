28995dc2fb06242d9645401865064b4787989fca
iarichandra <38314579+iarichandra@users.noreply.github.com>
2018-04-12 18:22:01 +0530

    Update loginController.js

--- js/login/controller/loginController.js
+++ js/login/controller/loginController.js
@@ -18,14 +18,23 @@ alert("Login");
 			    $scope.checkLoginState();
 		    	}, {scope: 'public_profile,email'});
 	};
+	
+	 (function(d, s, id){
+		     var js, fjs = d.getElementsByTagName(s)[0];
+		     if (d.getElementById(id)) {return;}
+		     js = d.createElement(s); js.id = id;
+		     js.src = "https://connect.facebook.net/en_US/sdk.js";
+		     fjs.parentNode.insertBefore(js, fjs);
+		   }(document, 'script', 'facebook-jssdk'));
 		  
-		  $scope.checkLoginState = function() {
-			    FB.getLoginStatus(function(response) {
-			    	console.log("login status response");
-			    	 console.log(response);
-			      $scope.statusChangeCallback(response);
-			    });
-			}
+		   $scope.testAPI=function() {
+		    console.log('Welcome!  Fetching your information.... ');
+		    FB.api('/me', function(response) {
+		      console.log('Successful login for: ' + response.name);
+		      document.getElementById('status').innerHTML =
+		        'Thanks for logging in, ' + response.name + '!';
+		    });
+		  };
 		  
 		  $scope.statusChangeCallback = function(response) {
 			    console.log('statusChangeCallback');
@@ -42,23 +51,18 @@ alert("Login");
 			      document.getElementById('status').innerHTML = 'Please log ' +
 			        'into this app.';
 			    }
-			  }
+			  };
+	
+	$scope.checkLoginState = function() {
+			    FB.getLoginStatus(function(response) {
+			    	console.log("login status response");
+			    	 console.log(response);
+			      $scope.statusChangeCallback(response);
+			    });
+			};
 
-		  (function(d, s, id){
-		     var js, fjs = d.getElementsByTagName(s)[0];
-		     if (d.getElementById(id)) {return;}
-		     js = d.createElement(s); js.id = id;
-		     js.src = "https://connect.facebook.net/en_US/sdk.js";
-		     fjs.parentNode.insertBefore(js, fjs);
-		   }(document, 'script', 'facebook-jssdk'));
+		 
 		   
-		   $scope.testAPI=function() {
-		    console.log('Welcome!  Fetching your information.... ');
-		    FB.api('/me', function(response) {
-		      console.log('Successful login for: ' + response.name);
-		      document.getElementById('status').innerHTML =
-		        'Thanks for logging in, ' + response.name + '!';
-		    });
-		  }
+		  
   
 });
